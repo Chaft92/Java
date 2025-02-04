f997b3dd08f5b93bcf9a982584cfb5dd1b43fa35
raymont dupont <julienyildiz92@gmail.com>
2025-02-04 17:02:42 +0100

    Premier commit

---
+++ ask.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+    
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello, " + name + " you are " + age + " years old.");
+        if (age < 18) {
+            System.out.println("You are a minor.");
+        } else {
+            System.out.println("You are an adult.");
+        }
+
+        scanner.close();
+    }
+
+}
