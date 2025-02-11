bc48ba5179ff9a3c3c2236d8359166a915d76b13
Chaft92 <julienyildiz92@gmail.com>
2025-02-11 11:49:25 +0100

    Cours 4 ?

---
+++ Produit.java
@@ -0,0 +1,28 @@
+public class Produit {
+    protected double prix;
+
+    public class Livre extends Produit {
+        public Livre(int p) {
+            prix = p;
+        }
+    }
+
+    public class DVD extends Produit {
+        public DVD(int p) {
+            prix = p;
+        }
+    }
+
+    public void calculerPrix(double prix, double multi) {
+        System.out.println("Le prix est de " + prix * multi + " euros");
+    }
+
+    public static void main(String[] args) {
+        Produit produit = new Produit();
+        Livre livre = produit.new Livre(23);
+        DVD dvd = produit.new DVD(19);
+        livre.calculerPrix(livre.prix, 1.05);
+        dvd.calculerPrix(dvd.prix, 1.20);
+    }
+    
+}
